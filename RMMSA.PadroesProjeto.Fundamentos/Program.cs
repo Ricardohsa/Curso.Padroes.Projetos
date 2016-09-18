41d077bf38073e84ad170f4782cfb0687cea8edf
Ricardo Humberto Sa <r.humberto.sa@gmail.com>
2016-09-18 18:38:20 -0300

    Add project files.

---
+++ RMMSA.PadroesProjeto.Fundamentos/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace RMMSA.PadroesProjeto.Fundamentos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var Calc = new Calculadora();
+
+            int x = 10;
+            int y = 20;
+            int r1 = Calc.Somar (x, y);
+
+            int v1 = 56;
+            int v2 = 90;
+            int r2 = Calc.Somar(v1, v2);
+
+            Console.WriteLine("A soma de x e y é : " + r1);
+            Console.WriteLine("A soma de x e y é : " + r2);
+            Console.ReadLine();
+        }
+
+      
+    }
+}
