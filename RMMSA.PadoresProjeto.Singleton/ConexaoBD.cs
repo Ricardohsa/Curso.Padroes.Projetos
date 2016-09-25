90d4ea29cec2d6527b5642a1d12fa446191fc397
Ricardo Humberto Sa <r.humberto.sa@gmail.com>
2016-09-25 15:53:02 -0300

    Finalizado os Padroes de Criação - com o Singleton

---
+++ RMMSA.PadoresProjeto.Singleton/ConexaoBD.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMMSA.PadoresProjeto.Singleton
+{
+    public class ConexaoBD
+    {
+        //guarda internamente a instance
+        private static ConexaoBD _instance;
+
+        protected ConexaoBD()
+        {
+        }
+
+        public static ConexaoBD Instance()
+        {
+            if (_instance == null)
+                _instance = new ConexaoBD();
+
+            return _instance; 
+        }
+
+        public string  StringConexao { get; set; }
+
+        public void Open()
+        {
+            Console.WriteLine("Abrindo conexão com Banco de Dados...." + StringConexao);
+        }
+
+
+    }
+}
---
+++ RMMSA.PadoresProjeto.Singleton/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMMSA.PadoresProjeto.Singleton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var conn = ConexaoBD.Instance();
+            conn.StringConexao = "Oracle";
+            conn.Open();
+
+            var conn2 = ConexaoBD.Instance();
+            //conn2.StringConexao = "SQL SERVER";
+            conn2.Open();
+
+            Console.ReadLine();
+        }
+    }
+}
