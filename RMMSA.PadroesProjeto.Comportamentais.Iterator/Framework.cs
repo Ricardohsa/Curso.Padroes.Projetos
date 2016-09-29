5accd18fe4a481d77544ba52a9204d3bd778f996
Ricardo Humberto Sa <r.humberto.sa@gmail.com>
2016-09-29 13:50:19 -0300

    Iterator

---
+++ RMMSA.PadroesProjeto.Comportamentais.Iterator/Framework.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMMSA.PadroesProjeto.Comportamentais.Iterator
+{
+    //Iterator
+    public abstract class Iterator
+    {
+        public abstract object First();
+        public abstract Object Next();
+        public abstract bool IsDone();
+        public abstract Object CurrenItem();
+    }
+
+    //Aggrefate
+    public abstract class Aggregate
+    {
+        public abstract Iterator CreaIterator { get; }
+    }
+
+    //Concrete Aggregate
+    public class Equipe : Aggregate
+    {
+        private List<string> _items = new List<string>();
+
+        public override Iterator CreaIterator
+        {
+            get
+            {
+                return new ConcreteIterator(this);
+            }
+        }
+
+        public int Count
+        {
+            get { return _items.Count; }
+        }
+
+        //indexer
+        public object this[int index]
+        {
+            get { return _items[index]; }
+            set { _items.Insert(index, (string) value);}
+        }
+       
+    }
+
+    //Concrete Iteragor
+    public class ConcreteIterator : Iterator
+    {
+        private Equipe _aggregate;
+
+        private int current = 0;
+
+        public ConcreteIterator(Equipe aggregate)
+        {
+            _aggregate = aggregate;
+        }
+
+        public override object First()
+        {
+            return _aggregate[0];
+        }
+
+        public override object Next()
+        {
+            object ret = null;
+            if (current < _aggregate.Count - 1)
+                ret = _aggregate[++current];
+            return ret;
+        }
+
+        public override bool IsDone()
+        {
+            return current >= _aggregate.Count;
+        }
+
+        public override object CurrenItem()
+        {
+            return _aggregate[current];
+        }
+    }
+
+}
---
+++ RMMSA.PadroesProjeto.Comportamentais.Iterator/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RMMSA.PadroesProjeto.Comportamentais.Iterator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Cria Concrete Aggregate
+            var equipe = new Equipe();
+            equipe[0] = "Ricardo";
+            equipe[1] = "Miguel";
+            equipe[2] = "Michellle";
+            equipe[3] = "Sá";
+
+            //Cria Iterator
+            ConcreteIterator i = new ConcreteIterator(equipe);
+
+            //iterar pela coleção
+            Console.WriteLine("Listando membros da equipe:");
+            object item = i.First();
+
+
+            while (item != null)
+            {
+                Console.WriteLine(item);
+                item = i.Next();
+            }
+
+            Console.ReadLine();
+
+        }
+    }
+}
